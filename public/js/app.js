ae0a4ae7d53905354543b0fbc8bcb9c3c6d5fdf5
Christian Rittmann <crittmann@hotmail.comgit config --global user.name Christian>
2018-05-30 00:06:27 -0500

    2nd commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,5 @@
 // Grab the articles as a json
-$("#scrapeButton").on("click", getArticles())
-
-//function to be called on click of button to gather list of articles
-function getArticles() {
+$("#scrapeButton").on("click", function() {
     $.ajax({
         method: "GET",
         url: "/articles",
@@ -16,14 +13,31 @@ function getArticles() {
             return data;
         }
     })
-};
-$.getJSON("/articles", function (data) {
-    // For each one
-    for (var i = 0; i < data.length; i++) {
-        // Display the apropos information on the page
-        $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-    }
 });
+
+// //function to be called on click of button to gather list of articles
+// function getArticles() {
+//     $.ajax({
+//         method: "GET",
+//         url: "/articles",
+//         data: function (data) {
+//             // For each one
+//             for (var i = 0; i < data.length; i++) {
+//                 // Display the information on the page
+//                 $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>" + 
+//             "<button id=saveButton>" + "save" + "</button>" );
+//             };
+//             return data;
+//         }
+//     })
+// };
+// $.getJSON("/articles", function (data) {
+//     // For each one
+//     for (var i = 0; i < data.length; i++) {
+//         // Display the apropos information on the page
+//         $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+//     }
+// });
 
 
 // Whenever someone clicks a p tag
